5af50c2f4f8c1b479008a18720cd41057400cb48
sungwoo1548 <swo1548@gmail.com>
2019-09-03 20:50:30 +0900

    set app.js

--- dop_zero/app.js
+++ dop_zero/app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejsLocals = require('ejs-locals'); // ejs-locals 사용
 
 var app = express();
 
 // view engine setup
+app.engine('ejs', ejsLocals); // view engine 설정
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,14 +20,15 @@ app.use(cookieParser());
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/dop', require('./routes/dopRouter')); // .../dop url 접속 처리
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
